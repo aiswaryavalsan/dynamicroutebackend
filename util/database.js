2bdede895a8c9bf862e91561fcda89f23aa05216
aiswarya <aiswaryavalsan@gmail.com>
2024-03-17 15:53:27 +0530

    connecting database to node

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const mysql=require('mysql2');
+const pool=mysql.createPool({
+    host:'localhost',
+    user:'root',
+    database:'node-complete',
+    password:'1234567',
+});
+module.exports=pool.promise();
